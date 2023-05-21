21cb28de150ab28a2441b64f875f998e99b96617
eduardoqsilva <eduadsilva@outlook.com>
2023-05-21 20:54:22 -0300

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { GlobalStyled } from './styles/Global.styled.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyled />
     <App />
   </React.StrictMode>,
 )
