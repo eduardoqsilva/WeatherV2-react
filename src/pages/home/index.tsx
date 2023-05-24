fdec9e718d7561049b722cad6c1281370aefb8e2
eduardoqsilva <eduadsilva@outlook.com>
2023-05-23 22:56:14 -0300

    update pages for tests

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,15 @@
+import { useFetchCurrent } from "../../services/api/useFetchCurrent"
+import { IconSelect } from "../../components/IconSelect";
+
+export function Home() {
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // const {data, isFetching} = useFetchCurrent()
+
+  return (
+    <div>
+      {/* <h1>Fetching: {isFetching.toString()}</h1>
+      <IconSelect iconName={data?.weather[0].icon}/> */}
+    </div>
+  )
+}
