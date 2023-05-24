8d4b10061159261f7578f14931703398096a1e02
eduardoqsilva <eduadsilva@outlook.com>
2023-05-24 18:49:13 -0300

    create a component current card

---
+++ src/components/CurrentCard/currentCard.styled.ts
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+import { ThemeDefault } from "../../styles/themes/default";
+
+const colors = ThemeDefault.colors
+const font_size = ThemeDefault.font_size
+
+export const ContainerCurrentCard = styled.div`
+  width: 100%;
+  max-width: 500px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+
+  position: relative;
+  z-index: 10;
+
+  & h3 {
+    font-size: ${font_size.XL}
+  }
+  & .temp {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 0.5rem;
+
+    & h1 {
+      font-size: 4rem;
+      font-weight: 500;
+      text-align: center;
+
+      & sup {
+        position: absolute;
+      }
+    }
+    & span {
+      font-size: 1rem;
+    }
+  }
+
+  &::before {
+    content: '';
+    width: 220px;
+    height: 220px;
+    
+    background-color: ${colors.green5};
+    border-radius: 50%;
+    filter: blur(70px);
+
+    position: absolute;
+    right: -10%;
+    top: 50%;
+    z-index: -1;
+    transform: translateY(-50%);
+  }
+`
+
+export const InfoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  & div.info {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+`
---
+++ src/components/CurrentCard/index.tsx
@@ -0,0 +1,51 @@
+import { Drop, Sun, Wind } from "@phosphor-icons/react";
+import { IconSelect } from "../IconSelect";
+import { ContainerCurrentCard, InfoWrapper } from "./currentCard.styled";
+import { ThemeDefault } from "../../styles/themes/default";
+
+const colors = ThemeDefault.colors
+
+interface CurrentType {
+  city: string | undefined
+  temp: number | undefined
+  desc: string | undefined
+  wind: number | undefined
+  umd: number | undefined
+  icon:  '01d' | '02d' | '03d' | '04d' | '09d' | '10d' | '11d' | '13d' | '50d' |
+  '01n' | '02n' | '03n' | '04n' | '09n' | '10n' | '11n' | '13n' | '50n'
+}
+
+export function CurrentCard({city, temp, desc, umd, wind, icon}:CurrentType) {
+
+  function Time() {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    return hours + ':' + minutes;
+  }
+
+  return (
+    <ContainerCurrentCard>
+      <h3>{city}</h3>
+      <IconSelect iconName={icon}/>
+      <div className="temp">
+        <h1>{temp ? Math.round(temp) : ''}<sup>°</sup></h1>
+        <span>{desc}</span>
+      </div>
+      <InfoWrapper>
+        <div className="info">
+          <Wind size={25} color={colors.white}/>
+          <span>{ wind ? Math.round(wind) : ''}km</span>
+        </div>
+        <div className="info">
+          <Drop size={25} color={colors.white}/>
+          <span>{umd}%</span>
+        </div>
+        <div className="info">
+          <Sun size={25} color={colors.white}/>
+          <span>{Time()}</span>
+        </div>
+      </InfoWrapper>
+    </ContainerCurrentCard>
+  )
+}
