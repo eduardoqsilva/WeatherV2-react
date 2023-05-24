d85306970b39862e4919e1815f8c679bb0e2688f
eduardoqsilva <eduadsilva@outlook.com>
2023-05-23 22:55:27 -0300

    update services/api

--- src/services/api/WeatherData.d.ts
+++ src/services/api/WeatherData.d.ts
@@ -1,6 +1,6 @@
 // Arquivo: weatherData.d.ts
 
-export interface WeatherData {
+export interface WeatherDataForecast {
   cod: string;
   message: number;
   cnt: number;
@@ -38,4 +38,49 @@ export interface WeatherData {
     };
     dt_txt?: string;
   }[];
+}
+
+
+export interface WeatherDataCurrent {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: '01d' | '02d' | '03d' | '04d' | '09d' | '10d' | '11d' | '13d' | '50d' | 
+    '01n' | '02n' | '03n' | '04n' | '09n' | '10n' | '11n' | '13n' | '50n';
+  }[];
+  base: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+  };
+  visibility: number;
+  wind: {
+    speed: number;
+    deg: number;
+    gust?: number;
+  };
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  sys: {
+    type?: number;
+    id?: number;
+    country?: string;
+    sunrise?: number;
+    sunset?: number;
+  };
+  timezone?: number;
+  id?: number;
+  name?: string;
+  cod?: number;
 }
