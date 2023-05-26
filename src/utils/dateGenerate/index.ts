ef86251c60c7831a8b45cef4e3f12b5a9f148cef
eduardoqsilva <eduadsilva@outlook.com>
2023-05-25 22:35:42 -0300

    create a create a function GetDatesArray

---
+++ src/utils/dateGenerate/index.ts
@@ -0,0 +1,15 @@
+export function GetDatesArray() {
+  const dates = [];
+
+  for (let i = 0; i < 5; i++) {
+    const date = new Date();
+    date.setDate(date.getDate() + i + 1);
+    date.setUTCHours(12 - 3, 0, 0, 0);
+  
+    const dayOfWeek = ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sáb'][date.getDay()];
+  
+    dates.push({ date: date.toISOString().slice(0, 19).replace('T', ' '), day: dayOfWeek });
+  }
+
+  return dates;
+}
