0217dc1089fed4c6ec36afa1192782fbbf6775b5
eduardoqsilva <eduadsilva@outlook.com>
2023-05-27 19:34:12 -0300

    update styled from IconSelect

--- src/components/IconSelect/iconSelect.styled.ts
+++ src/components/IconSelect/iconSelect.styled.ts
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const IconWrapper = styled.div`
-  width: 150px;
-  height: 150px;
+  width: 100%;
+  height: 100%;
+  max-width: 150px;
+  max-height: 150px;
 `
