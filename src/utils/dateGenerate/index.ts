97baab2258b5373cb23cf82be98a46e0a9547a47
eduardoqsilva <eduadsilva@outlook.com>
2023-05-31 20:50:39 -0300

    format code

--- src/utils/dateGenerate/index.ts
+++ src/utils/dateGenerate/index.ts
@@ -2,14 +2,14 @@ export function GetDatesArray() {
   const dates = [];
 
   for (let i = 0; i < 5; i++) {
-    const date = new Date();
-    date.setDate(date.getDate() + i + 1);
-    date.setUTCHours(12, 0, 0, 0);
-  
-    const dayOfWeek = ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sáb'][date.getDay()];
-  
-    dates.push({ date: date.toISOString().slice(0, 19).replace('T', ' '), day: dayOfWeek });
+    const date = new Date()
+    date.setDate(date.getDate() + i + 1)
+    date.setUTCHours(12, 0, 0, 0)
+
+    const dayOfWeek = ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sáb'][date.getDay()]
+
+    dates.push({ date: date.toISOString().slice(0, 19).replace('T', ' '), day: dayOfWeek })
   }
 
-  return dates;
+  return dates
 }
